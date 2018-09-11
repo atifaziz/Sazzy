b0a4892e88490810f7bd6f44aa3835f9c7b488b6
Atif Aziz <code@raboof.com>
2018-09-11 18:17:51 +0200

    Remove reundant parantheses

--- src/Program.cs
+++ src/Program.cs
@@ -99,7 +99,7 @@ namespace Sazzy
                 var result = new StringBuilder();
                 int b;
                 char character;
-                while ((b = stream.ReadByte()) >= 0 && ((character = (char) b) != '\n'))
+                while ((b = stream.ReadByte()) >= 0 && (character = (char) b) != '\n')
                 {
                     if (character != '\r' && character != '\n')
                         result.Append(character);
