c827444136998b2f66198824d27862d56c504594
Atif Aziz <code@raboof.com>
2018-09-11 18:13:24 +0200

    Extract main workhorse into standalone method

--- src/Program.cs
+++ src/Program.cs
@@ -7,83 +7,88 @@ namespace Sazzy
 
     static class Program
     {
-        enum State { Headers, ChunkSize, Body, End };
-
         static void Main(string[] args)
+        {
+            using (var stream = File.Open(args[0], FileMode.Open, FileAccess.Read))
+            using (var output = Console.OpenStandardOutput())
+                CopyHttpContent(stream, output);
+        }
+
+        enum State { Headers, ChunkSize, Body }
+
+        static void CopyHttpContent(Stream input, Stream output)
         {
             var chunked = false;
             var state = State.Headers;
             var chunkSize = 0;
             var contentLength = -1;
 
-            using (var stream = File.Open(args[0], FileMode.Open, FileAccess.Read))
-            using (var output = Console.OpenStandardOutput())
+            while (true)
             {
-                while (state != State.End)
+                switch (state)
                 {
-                    switch (state)
+                    case State.Headers:
                     {
-                        case State.Headers:
+                        var line = input.ReadLine();
+                        if (string.IsNullOrEmpty(line))
                         {
-                            var line = stream.ReadLine();
-                            if (string.IsNullOrEmpty(line))
-                            {
-                                state = chunked ? State.ChunkSize : State.Body;
-                            }
-                            else if (line.Equals("Transfer-Encoding: chunked", StringComparison.OrdinalIgnoreCase))
-                            {
-                                chunked = true;
-                            }
-                            else if (line.StartsWith("Content-Length:", StringComparison.OrdinalIgnoreCase))
-                            {
-                                contentLength = int.Parse(Regex.Match(line, @"(?<=Content-Length: )\d+$").Value);
-                            }
-
-                            break;
+                            state = chunked ? State.ChunkSize : State.Body;
                         }
-                        case State.ChunkSize:
+                        else if (line.Equals("Transfer-Encoding: chunked", StringComparison.OrdinalIgnoreCase))
                         {
-                            chunkSize = int.Parse(stream.ReadLine(), NumberStyles.HexNumber);
-                            state = chunkSize > 0 ? State.Body : State.End;
-                            break;
+                            chunked = true;
                         }
-                        case State.Body when chunked:
+                        else if (line.StartsWith("Content-Length:", StringComparison.OrdinalIgnoreCase))
                         {
-                            var buffer = new byte[chunkSize];
-
-                            while (chunkSize > 0)
-                            {
-                                var read = stream.Read(buffer, 0, buffer.Length);
-                                if (read == 0)
-                                    throw new Exception("Unexpected end of HTTP content.");
-                                output.Write(buffer, 0, read);
-                                chunkSize -= read;
-                            }
-
-                            var line = stream.ReadLine();
-                            if (!string.IsNullOrEmpty(line))
-                            {
-                                throw new Exception("Expected empty line but was " + line);
-                            }
-                            state = State.ChunkSize;
-                            break;
+                            contentLength = int.Parse(Regex.Match(line, @"(?<=Content-Length: )\d+$").Value);
                         }
-                        case State.Body:
+
+                        break;
+                    }
+                    case State.ChunkSize:
+                    {
+                        chunkSize = int.Parse(input.ReadLine(), NumberStyles.HexNumber);
+                        if (chunkSize == 0)
+                            return;
+                        state = State.Body;
+                        break;
+                    }
+                    case State.Body when chunked:
+                    {
+                        var buffer = new byte[chunkSize];
+
+                        while (chunkSize > 0)
                         {
-                            var buffer = new byte[contentLength];
-
-                            while (contentLength > 0)
-                            {
-                                var read = stream.Read(buffer, 0, buffer.Length);
-                                if (read == 0)
-                                    throw new Exception("Unexpected end of HTTP content.");
-                                output.Write(buffer, 0, read);
-                                contentLength -= read;
-                            }
-
-                            state = State.End;
-                            break;
+                            var read = input.Read(buffer, 0, buffer.Length);
+                            if (read == 0)
+                                throw new Exception("Unexpected end of HTTP content.");
+                            output.Write(buffer, 0, read);
+                            chunkSize -= read;
                         }
+
+                        var line = input.ReadLine();
+                        if (!string.IsNullOrEmpty(line))
+                        {
+                            throw new Exception("Expected empty line but was " + line);
+                        }
+
+                        state = State.ChunkSize;
+                        break;
+                    }
+                    case State.Body:
+                    {
+                        var buffer = new byte[contentLength];
+
+                        while (contentLength > 0)
+                        {
+                            var read = input.Read(buffer, 0, buffer.Length);
+                            if (read == 0)
+                                throw new Exception("Unexpected end of HTTP content.");
+                            output.Write(buffer, 0, read);
+                            contentLength -= read;
+                        }
+
+                        return;
                     }
                 }
             }
