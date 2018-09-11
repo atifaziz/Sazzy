18a2e7a81f545e73f33a8e66f7b2154781a651a1
Atif Aziz <code@raboof.com>
2018-09-11 16:14:15 +0200

    Initial project structure

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+namespace Sazzy
+{
+    using System;
+
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
