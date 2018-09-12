4591a5aea79886eefac7d02dbf882f5cafda8c26
Atif Aziz <code@raboof.com>
2018-09-12 09:56:41 +0200

    Add chunked transfer encoding test

--- tests/HttpContentStreamTests.cs
+++ tests/HttpContentStreamTests.cs
@@ -1,6 +1,8 @@
 namespace Sazzy.Tests
 {
     using System;
+    using System.IO;
+    using System.Text;
     using NUnit.Framework;
     using Sazzy;
 
@@ -12,6 +14,34 @@ namespace Sazzy.Tests
             var e = Assert.Throws<ArgumentNullException>(() =>
                 HttpContentStream.Open(null));
             Assert.That(e.ParamName, Is.EqualTo("input"));
+        }
+
+        [Test]
+        public void ChunkedTransferEncoding()
+        {
+            const string crlf = "\r\n";
+            const string response
+                = "HTTP/1.1 200 OK" + crlf
+                + "Content-Type: text/plain" + crlf
+                + "Transfer-Encoding: chunked" + crlf
+                + "" + crlf
+                + "7" + crlf
+                + "Mozilla" + crlf
+                + "9" + crlf
+                + "Developer" + crlf
+                + "7" + crlf
+                + "Network" + crlf
+                + "0" + crlf
+                + crlf;
+
+            var ascii = Encoding.ASCII;
+            var input = new MemoryStream(ascii.GetBytes(response));
+            var hs = HttpContentStream.Open(input);
+            var output = new MemoryStream();
+            hs.CopyTo(output);
+            var content = ascii.GetString(output.ToArray());
+
+            Assert.That(content, Is.EqualTo("MozillaDeveloperNetwork"));
         }
     }
 }
