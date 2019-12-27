62b24c5f6e0fd7fcc6849b0dd047fc74e59fa08a
Atif Aziz <code@raboof.com>
2019-12-27 13:40:08 +0100

    Remove unused import

--- tests/HttpMessageTests.cs
+++ tests/HttpMessageTests.cs
@@ -2,7 +2,6 @@ namespace Sazzy.Tests
 {
     using System;
     using System.Collections.Generic;
-    using System.Globalization;
     using System.IO;
     using System.Linq;
     using System.Net;
