4212dbee4246163c28be8393566f72dee8971aa4
Atif Aziz <code@raboof.com>
2020-01-22 13:44:37 +0100

    Merge values of duplicate HTTP headers

--- src/HttpMessage.cs
+++ src/HttpMessage.cs
@@ -69,7 +69,49 @@ namespace Sazzy
                 {
                     _headerByName =
                         Headers.GroupBy(e => e.Key, e => e.Value, StringComparer.OrdinalIgnoreCase)
-                               .ToDictionary(g => g.Key, g => g.Last(), StringComparer.OrdinalIgnoreCase);
+                               .ToDictionary(g => g.Key,
+                                             //
+                                             // RFC 2616
+                                             // http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.2
+                                             //
+                                             // Multiple message-header fields with the same field-name
+                                             // MAY be present in a message if and only if the entire
+                                             // field-value for that header field is defined as a
+                                             // comma-separated list [i.e., #(values)]. It MUST be
+                                             // possible to combine the multiple header fields into one
+                                             // "field-name: field-value" pair, without changing the
+                                             // semantics of the message, by appending each subsequent
+                                             // field-value to the first, each separated by a comma.
+                                             // The order in which header fields with the same
+                                             // field-name are received is therefore significant to the
+                                             // interpretation of the combined field value, and thus a
+                                             // proxy MUST NOT change the order of these field values
+                                             // when a message is forwarded.
+                                             //
+                                             // RFC 7230, Section 3.2.2: Field Order
+                                             // https://tools.ietf.org/html/rfc7230#section-3.2.2
+                                             //
+                                             // Note: In practice, the "Set-Cookie" header field
+                                             // ([RFC6265]) often appears multiple times in a response
+                                             // message and does not use the list syntax, violating the
+                                             // above requirements on multiple header fields with the
+                                             // same name. Since it cannot be combined into a single
+                                             // field-value, recipients ought to handle "Set-Cookie"
+                                             // as a special case while processing header fields.
+                                             //
+                                             // Using the exception list from "nsHttpHeaderArray.h"
+                                             // from mozilla-central:
+                                             //
+                                             // https://github.com/bnoordhuis/mozilla-central/blob/c41009f7ae12524a21f8178fdbcc72ebf9a35fcf/netwerk/protocol/http/nsHttpHeaderArray.h#L185-L187
+                                             //
+                                             g => string.Join(   string.Equals(g.Key, "Set-Cookie"        , StringComparison.OrdinalIgnoreCase)
+                                                              || string.Equals(g.Key, "WWW-Authenticate"  , StringComparison.OrdinalIgnoreCase)
+                                                              || string.Equals(g.Key, "Proxy-Authenticate", StringComparison.OrdinalIgnoreCase)
+                                                              ? "\n" : ",",
+                                                              from v in g
+                                                              where !string.IsNullOrWhiteSpace(v)
+                                                              select v),
+                                             StringComparer.OrdinalIgnoreCase);
                 }
 
                 return _headerByName.TryGetValue(header, out var value) ? value : null;
